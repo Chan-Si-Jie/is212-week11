0370e80907622e98df3990fae7e5d61024f74f8d
Chan-Si-Jie <sijie.chan.2023@scis.smu.edu.sg>
2025-11-30 15:11:51 +0800

    refactor code

--- ex3/ex3_smelly.py
+++ ex3/ex3_smelly.py
@@ -1,51 +1,37 @@
-class Electronics:
+class Good:
+    type = "Good"
+    discount = 0
+    tax_rate = 0
+    
     def __init__(self, name, price):
         self.name = name
         self.price = price
     
     def apply_discount(self):
-        discount = 0.10  # 10% discount
-        discounted_price = self.price - (self.price * discount)
-        print(f"Discounted price for {self.name} (Electronics): {discounted_price}")
+        discounted_price = self.price - (self.price * self.discount)
+        print(f"Discounted price for {self.name} ({self.type}): {discounted_price}")
         return discounted_price
     
     def calculate_tax(self):
-        tax_rate = 0.15  # 15% tax
-        tax = self.price * tax_rate
-        print(f"Tax for {self.name} (Electronics): {tax}")
+        tax = self.price * self.tax_rate
+        print(f"Tax for {self.name} ({self.type}): {tax}")
         return tax
 
-class Clothing:
-    def __init__(self, name, price):
-        self.name = name
-        self.price = price
 
-    def apply_discount(self):
-        discount = 0.20  # 20% discount
-        discounted_price = self.price - (self.price * discount)
-        print(f"Discounted price for {self.name} (Clothing): {discounted_price}")
-        return discounted_price
+class Electronics(Good):
+    type = "Electronics"
+    discount = 0.10
+    tax_rate = 0.15
     
-    def calculate_tax(self):
-        tax_rate = 0.08  # 8% tax
-        tax = self.price * tax_rate
-        print(f"Tax for {self.name} (Clothing): {tax}")
-        return tax
+    
 
-class Grocery:
-    def __init__(self, name, price):
-        self.name = name
-        self.price = price
-    
-    def apply_discount(self):
-        discount = 0.05  # 5% discount
-        discounted_price = self.price - (self.price * discount)
-        print(f"Discounted price for {self.name} (Grocery): {discounted_price}")
-        return discounted_price
-    
-    def calculate_tax(self):
-        tax_rate = 0.02  # 2% tax
-        tax = self.price * tax_rate
-        print(f"Tax for {self.name} (Grocery): {tax}")
-        return tax
+class Clothing (Good):
+    type = "Clothing"
+    discount = 0.20
+    tax_rate = 0.08
+
+class Grocery(Good):
+    type="Grocery"
+    discount = 0.05
+    tax_rate = 0.02
 
